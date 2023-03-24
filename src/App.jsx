d2d4c2937e8850ff5a407853c4fd9d9cd018c3d5
majazocom <70019958+majazocom@users.noreply.github.com>
2023-03-24 12:06:29 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,30 +4,43 @@ import PokemonsList from './Components/PokemonsList';
 
 function App() {
   let availablePokemonsList =
-    [
-      {
-        id: 1,
-        name: 'Bulbasaur',
-        cp: 439
-      },
-      {
-        id: 2,
-        name: 'Vulpix',
-        cp: 194
-      },
-      {
-        id: 3,
-        name: 'Pikachu',
-        cp: 455
-      },
-      {
-        id: 4,
-        name: 'Gyarados',
-        cp: 2243
-      }
-    ];
+    [{
+      id: 1,
+      name: 'Bulbasaur',
+      cp: 439
+    },
+    {
+      id: 2,
+      name: 'Vulpix',
+      cp: 194
+    },
+    {
+      id: 3,
+      name: 'Pikachu',
+      cp: 455
+    },
+    {
+      id: 4,
+      name: 'Gyarados',
+      cp: 2243
+    }];
   const [availablePokemons, setAvailablePokemons] = useState(availablePokemonsList);
   const [chosenPokemons, setChosenPokemons] = useState([]);
+
+  function addToChosenPokemons(pokemon) {
+    setChosenPokemons(
+      [...chosenPokemons,
+        pokemon]
+    );
+    setAvailablePokemons(
+      availablePokemons.filter((ap) => ap.id !== pokemon.id)
+    )
+  };
+
+  function removeFromChosenPokemons(pokemon) {
+    console.log(pokemon);
+  }
+
   return (
     <>
       <header>
@@ -36,8 +49,8 @@ function App() {
       <main>
         <h2>Choose your champions</h2>
         <section className='pokemonsListsContainer'>
-          <PokemonsList pokemons={availablePokemons} title="Available Pokémons" />
-          <PokemonsList pokemons={chosenPokemons} title="Chosen Pokémons" />
+          <PokemonsList pokemons={availablePokemons} title="Available Pokémons" handleClick={addToChosenPokemons} />
+          <PokemonsList pokemons={chosenPokemons} title="Chosen Pokémons" handleClick={removeFromChosenPokemons} />
         </section>
       </main>
     </>
