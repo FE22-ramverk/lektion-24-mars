4986e755c1805865f9346b2a6171ff75eb113f34
majazocom <70019958+majazocom@users.noreply.github.com>
2023-03-24 12:07:02 +0100

    Update PokemonsList.jsx

--- src/Components/PokemonsList.jsx
+++ src/Components/PokemonsList.jsx
@@ -1,15 +1,24 @@
 import PokemonCard from "./PokemonCard";
 
-function PokemonsList({pokemons, title}) {
-    console.log(pokemons);
+function PokemonsList({ pokemons, title, handleClick }) {
+
+    function calcCP() {
+        let totalCP = 0;
+        pokemons.forEach(p => {
+            totalCP += p.cp
+        });
+        return totalCP;
+    }
+
     return (
         <section className="pokemonsListContainer">
             <h3>{title}</h3>
-                {
-                    pokemons.map((pokemon, i) => (
-                        <PokemonCard key={i} pokemon={pokemon} />
-                    ))
-                }
+            {
+                pokemons.map((pokemon, i) => (
+                    <PokemonCard key={i} pokemon={pokemon} handleClick={handleClick} />
+                ))
+            }
+            <p>{calcCP()}CP</p>
         </section>
     )
 };
